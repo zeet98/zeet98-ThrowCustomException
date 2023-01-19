a12e4d9fe5ea8cb272c1e08e7c311e24f0a082f4
tedbeast <theodore.balashov@revature.com>
2023-01-19 14:42:21 -0500

    added useful comment

--- src/main/java/CustomException.java
+++ src/main/java/CustomException.java
@@ -3,6 +3,8 @@
  * This class represents a custom exception which may be thrown in the CustomExceptionThrower class.
  * By extending Exception, CustomException has inherited all the behavior of Exception while being its own class!
  * Potentially, this class could also override some the behavior of Exception.
+ *
+ * There is no need to modify this class.
  */
 public class CustomException extends Exception{
 }
--- src/main/java/CustomExceptionThrower.java
+++ src/main/java/CustomExceptionThrower.java
@@ -4,9 +4,16 @@ public class CustomExceptionThrower {
     /**
      * This method should throw a CustomException. CustomException is a custom exception that we've written ourselves
      * by extending the Exception class in CustomException.java. In a large scale application, writing custom
-     * exceptions could provide valuable information for when something goes wrong in the application.
+     * exceptions could provide valuable information for when something goes wrong in the application. For instance,
+     * if the development of a new method would require a FileNotFoundException to be thrown in a certain situation,
+     * the line throw new FileNotFoundException();
      *
-     * I recommend looking at the test case for a good example of try/catch block usage, as well.
+     * You will need to add a 'throws CustomException' declaration to this method, and throw a new CustomException
+     * within this method.
+     *
+     * This also means that you should NOT write a try/catch block in this method, as the tests are expecting to have
+     * a method thrown to it. A try/catch block would handle the exception within the method. The tests, on the other
+     * hand, are using a try/catch block to catch the exception thrown by this method.
      */
     public void throwCustomException(){
 
