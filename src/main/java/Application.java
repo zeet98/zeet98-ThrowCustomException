b330390f3d0afd5c234367634d9c1e2b57745355
tedbeast <theodore.balashov@revature.com>
2023-02-16 13:05:53 -0500

    added throws declaration

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,7 +6,7 @@ public class Application {
      *
      * The Lab.mustThrow method should throw the CustomException class.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CustomException{
         Lab thrower = new Lab();
         System.out.println("The lab code should throw a CustomException, causing a stack trace to be printed to console here:");
         thrower.throwCustomException();
