30c6f1e760c810c07cb33af0c37dc2ba9400e764
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-28 17:47:48 +0000

    Complete CustomExc assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,7 +15,8 @@ public class Lab {
      * a method thrown to it. A try/catch block would handle the exception within the method. The tests, on the other
      * hand, are using a try/catch block to catch the exception thrown by this method.
      */
-    public void throwCustomException(){
+    public void throwCustomException() throws CustomException{
+        throw new CustomException();
 
     }
 
